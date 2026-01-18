0dfce1bcfedcd29659d87ba5c4a04cf612cecff2
Manjesh patil <manjesh.patil@asp.com>
2026-01-18 23:50:02 +0530

    understanding Open close principle

---
+++ design_principles/open_close_priciple/ocp.cpp
@@ -0,0 +1,111 @@
+/*
+Problem statement: We are building a Notification system that sends messages via:
+Email
+SMS
+(Later) WhatsApp, Push, etc.
+*/
+
+
+#include <iostream>
+#include <string>
+
+
+// Old code without OCP 
+// class NotificationService {
+// public:
+//     void send(const std::string& type, const std::string& message) {
+//         if (type == "EMAIL") {
+//             printf("This is from Email service \n");
+//         }
+//         else if (type == "SMS") {
+//             printf("This is from SMS service \n");
+//         }
+//         // Tomorrow:
+//         // else if (type == "WHATSAPP") { ... }
+//     }
+// };
+
+
+
+//Now via OCP principle
+class Notification {
+
+    public:
+    virtual ~Notification() = default;
+    virtual void send(const std::string & message) = 0;
+
+};
+
+class EmailNotification: public Notification {
+
+    public:
+    void display()
+    {
+        printf("This is from EMAIL derived class \n");
+    }
+
+    void hello()
+    {
+        printf("This is dummy class \n");
+    }
+
+    void send(const std::string& message) override
+    {
+        std::cout << "Message sent via Email: " << message << std::endl;
+    }
+};
+
+class SMSNotification: public Notification {
+
+
+    public:
+
+    void send(const std::string& message) override
+    {
+       std::cout << "Message sent via SMS: " << message << std::endl;
+    }
+};
+
+// In future i can add for whatsapp, twitter etc...
+
+
+
+// Abstraction
+
+class NotificationService {
+public:
+    void notify(Notification& notification, const std::string& message) {
+        notification.send(message);
+    }
+};
+
+
+
+int main() {
+    // // calling via the stack
+    // NotificationService service;
+    // service.send("EMAIL", "Hello");
+
+    // // calling via pointers
+    // NotificationService* ser = new NotificationService();
+    // ser->send("SMS", "Hello from SMS");
+
+    Notification* noti_ptr = new EmailNotification();
+    EmailNotification* noti1_ptr = new EmailNotification();
+    noti_ptr->send("My mail is manjesh@gmail.com");
+    //noti1_ptr->hello();
+
+    Notification* SMS =  new SMSNotification();
+    SMS->send("OTP: 1234");
+
+    // Proper OCP
+
+    NotificationService service;
+
+    EmailNotification email;
+    SMSNotification sms;
+
+    service.notify(email, "Order confimed");
+    service.notify(sms, "OTP: 1234");
+    return 0;
+}
